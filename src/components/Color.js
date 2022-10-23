371da2ac7809fd464d9cd71d2b0daa368f2b1e54
RamS24 <AlSaadi.Rami24@gmail.com>
2022-10-23 10:34:43 +0200

    update redirecting

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,31 +1,17 @@
-import React, {useEffect} from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-
-
-
+import React from 'react';
+import {Navigate, useNavigate, useParams } from 'react-router-dom';
 
 const Color = () => {
     const navigate = useNavigate();
     const {colorName} = useParams();
     let bgColor = '#000';
+    const colorArr = JSON.parse(localStorage.getItem('colorsArray'));
+    const color = colorArr?.find(color=>color.name===colorName);
+    if (colorArr && color)
+      bgColor=(color.color);
+    else
+      return <Navigate to='./colors' replace/>
 
-    const getColor = () => {
-      const colorArr = JSON.parse(localStorage.getItem('colorsArray'));
-      if(!colorArr)
-        {
-          navigate('/colors');
-        }
-        else {
-      const color = colorArr.find(color=>color.name===colorName);
-      if (color)
-        bgColor=(color.color);
-      else
-        navigate('/colors');
-      }
-      console.log('Runs')
-    }
-    getColor();
-//console.log(bgColor);
   return (
     <div className='color' style={{backgroundColor: bgColor}}>
         <p>This is color {colorName}</p>
--- src/components/Colors.js
+++ src/components/Colors.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom'; 
 
 const Colors = ({colors, refreshColors}) => {
-    console.log(colors);
+ //   console.log(colors);
   return (
     <div className='colors'>
     <header>
